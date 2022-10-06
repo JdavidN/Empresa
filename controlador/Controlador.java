33cedb98eced9f64d511bf974103eba0c41ba0b9
Javier David Nino Ruiz <103191427+JdavidN@users.noreply.github.com>
2022-10-06 09:13:14 -0500

    Add files via upload

--- controlador/Controlador.java
+++ controlador/Controlador.java
@@ -5,6 +5,8 @@ import vista.VentanaPrincipal;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.border.EmptyBorder;
+
 
 public class Controlador implements ActionListener
 {
@@ -34,10 +36,26 @@ public class Controlador implements ActionListener
         //Identificar el comando generado (hallarMayor, borrar, salir)
         String comando = ae.getActionCommand();
         
-        if(comando.equals("Crear"))
+        if(comando.equals("Empleado"))
+        
+        
         {
+            String nomEmpleado = venPrin.miPanelEntradaDatos.getTfNomEmpleado();
+            String apeEmpleado = venPrin.miPanelEntradaDatos.getTfNomEmpleado();
+            int numHoras = Integer.parseInt(venPrin.miPanelEntradaDatos.getTfNumHoras());
+            int fecNacimiento = Integer.parseInt(venPrin.miPanelEntradaDatos.getTfFechaNacimiento());
 
-        }
+            //Creación del objeto tipo Carro
+            model = new Empleado(nomEmpleado, apeEmpleado, numHoras, fecNacimiento );
+                
+            venPrin.miPanelResultado.mostrarResultado("Se ha creado un Empleado");
+            venPrin.miPanelResultado.mostrarResultado("Su nombre es " + model.getNomEmpleado());
+            venPrin.miPanelResultado.mostrarResultado("Su apellido es " + model.getApeEmpleado());
+            venPrin.miPanelResultado.mostrarResultado("Sus Horas de trabajo en el mes (" + model.getNumHoras()+")" );
+            venPrin.miPanelResultado.mostrarResultado("Su Edad " + model.getFechaNacimiento());
+        } 
+          
+
         if(comando.equals("borrar"))
         {
 
